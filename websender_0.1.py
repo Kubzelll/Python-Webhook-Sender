2b2d86a46bfc5f4372aba7401634575ea9c59c88
Kubzel <110478750+Kubzelll@users.noreply.github.com>
2022-11-05 18:42:58 +0100

    Add files via upload
    Python script

---
+++ websender_0.1.py
@@ -0,0 +1,65 @@
+#Project made by Kubzel
+'''
+
+$$\   $$\          $$\                           $$\ 
+$$ | $$  |         $$ |                          $$ |
+$$ |$$  /$$\   $$\ $$$$$$$\  $$$$$$$$\  $$$$$$\  $$ |
+$$$$$  / $$ |  $$ |$$  __$$\ \____$$  |$$  __$$\ $$ |
+$$  $$<  $$ |  $$ |$$ |  $$ |  $$$$ _/ $$$$$$$$ |$$ |
+$$ |\$$\ $$ |  $$ |$$ |  $$ | $$  _/   $$   ____|$$ |
+$$ | \$$\\$$$$$$  |$$$$$$$  |$$$$$$$$\ \$$$$$$$\ $$ |
+\__|  \__|\______/ \_______/ \________| \_______|\__|
+                                                     
+
+'''
+from discord import Webhook, SyncWebhook
+
+
+
+
+def webhook_session(url):
+    requestmes = SyncWebhook.from_url(url)
+    print('Press CTRL+C TO EXIT')
+    print('Enter Text')
+    while True:
+        meesage = (input())
+        requestmes.send(meesage)
+    
+
+def main():
+    web_url = input("Input webhook url:")
+    webhook_session(web_url)
+
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+        
+
+
+
+
+        
+
+
+
+
+
+
+
+    
+
+
